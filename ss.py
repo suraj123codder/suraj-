0f62a285f2ed77f89c324cbb122866a8b6c63519
Suraj Gowda <35725087+suraj123codder@users.noreply.github.com>
2018-01-31 11:23:03 +0530

    Add files via upload

---
+++ ss.py
@@ -0,0 +1,20 @@
+# In this program,
+# we check if the number is positive or
+# negative or zero and
+# display an appropriate message
+
+num = 3.4
+
+# try these two variation as well:
+# num = 0
+# num = -4.5
+
+if num > 0:
+    print("positive number")
+elif num == 0:
+    print("zero")
+else:
+    print("negative")
+    
+
+    
