a0cc081df4d781e04665c422076da897c98ee2b3
Suraj Gowda <35725087+suraj123codder@users.noreply.github.com>
2018-01-31 11:22:00 +0530

    Add files via upload

---
+++ ssssu.py
@@ -0,0 +1,13 @@
+# program checks if the number is positive  or negative
+# And displays an appropriate message
+
+num = 3
+
+# Try these two variation as well.
+# num = -5
+# num = 0
+
+if num >= 0:
+    print("positive or zero")
+else:
+    print("negative number")
